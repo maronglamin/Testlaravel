234f30fa8e0c3e4bb17a47294511fd323c71fc5e
modoulamin <mlmarong14036@gmail.com>
2021-06-30 11:37:54 +0000

    my tutorial on laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>posty</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex item-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+        <ul class="flex item-center">
+            <li>
+                <a href="" class="p-3">Modou lamin</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">logout</a>
+            </li>
+        </ul>
+    </nav>
+    @yield("content")
+</body>
+
+</html>
